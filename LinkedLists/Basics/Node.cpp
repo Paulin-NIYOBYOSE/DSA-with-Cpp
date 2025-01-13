b849895474ee3c3b3d957080b732dca1933a4616
Paulin-NIYOBYOSE <niyobyosepaulin20@gmail.com>
2025-01-13 20:13:16 +0200

    Inserted node at the end

--- LinkedLists/Basics/Node.cpp
+++ LinkedLists/Basics/Node.cpp
@@ -12,3 +12,22 @@ void insertAtHead(Node*& head, int value) {
     newNode->next = head;        // Point to the current head
     head = newNode;              // Update the head to the new node
 }
+
+// Inserting node at the end
+void insertAtTail(Node*& head, int value) {
+    Node* newNode = new Node();
+    newNode->data = value;
+    newNode->next = nullptr;
+
+    if (head == nullptr) {  // If the list is empty
+        head = newNode;
+        return;
+    }
+
+    Node* temp = head;
+    while (temp->next != nullptr) {  // Traverse to the last node
+        temp = temp->next;
+    }
+    temp->next = newNode;  // Update the last node's pointer
+}
+
